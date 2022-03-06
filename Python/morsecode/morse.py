b206366c59c39f831371e0a90d09eec7f371b773
RobLangford <rob.langford1@gmail.com>
2022-03-06 19:03:36 +1000

    Added docstrings to functions to clarify functionality.

--- Python/morsecode/morse.py
+++ Python/morsecode/morse.py
@@ -48,13 +48,24 @@ morse = {
     "0": "-----",
 }
 
-# Declare length of the timings
+# Declare length of the timings for led blinks in seconds
 dit_length = 0.25
 dash_length = 0.75
 space_length = 1.5
 
 # function to get morse code from letter
 def get_morse(morse, key):
+    """
+    Converts input letters to morse code using the morse dictionary, if the space character is detected the string "===" is returned.
+
+
+    Args:
+        morse: passes the morse dictionary.
+        key: letter to be converted.
+
+    Returns:
+        function will either return the morse key for the input character or the === to denote a space.
+    """
     key = key.lower()
     if key in morse.keys():
         print(key +" = "+ morse[key])
@@ -66,6 +77,12 @@ def get_morse(morse, key):
 
 # function to convert morse 'dot' and 'dash' into LED output
 def led_morse(letter):
+    """
+    Simple function to iterate the morse key input and uses led.blink function to output on a raspberry pi project board via GPIO.
+    
+    Args: 
+        letter: string represents the morse key character as dots '.'  dashes '-'
+    """
     for morse_key in letter:
         if morse_key == ".":
             led.blink(on_time=dit_length,off_time=dit_length,n=1,background=False)
@@ -75,6 +92,11 @@ def led_morse(letter):
             sleep(space_length)
     
 def main():
+    """
+    Program converts a string of text input to morse code and then outputs via an led on a raspberry pi project board via GPIO.
+    See README on repo for details on the hardware used.
+
+    """
     # Read User input
     userInput = input("Input Text:")
 
